2627c77372642273efc7d1bd00589dd7960600e9
lucas <lcs.carvajal@gmail.com>
2022-06-26 17:30:27 +0200

    refactor requestHandler to split url to slice

--- requestHandler.go
+++ requestHandler.go
@@ -8,42 +8,56 @@ import (
 )
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.String()
-	//TODO remove trailing "/"
+	urlParts := strings.Split(r.URL.String(), "/")
+	urlParts = util.Filter(urlParts, func(s string) bool {
+		return s != ""
+	})
+	//TODO remove "" from urlParts
 	switch {
-	case url == "/":
+	case len(urlParts) == 0:
+		// default route "/"
 		doSomething(w, r)
 		return
-	case url == "/signup":
+	case urlParts[0] == "signup":
+		// signup route "/signup"
 		doSomething(w, r)
 		return
-	case url == "/login":
+	case urlParts[0] == "login":
+		// login route "/login"
 		doSomething(w, r)
 		return
-	case url == "/claim":
+	case urlParts[0] == "claim" && len(urlParts) == 1:
+		// claim route "/claim" -> file a claim
 		doSomething(w, r)
 		return
-	case strings.Contains(url, "/claim/"):
+	case urlParts[0] == "claim" && len(urlParts) > 1:
 		switch {
-		case strings.Contains(url, "/approve"):
+		case len(urlParts) == 2:
+			// claim id route "/claim/1424" -> retrieve details for id=1424
 			doSomething(w, r)
 			return
-		case strings.Contains(url, "/deny"):
+		case urlParts[2] == "approve" && len(urlParts) == 3:
+			// approve claim route "/claim/1424/approve"
 			doSomething(w, r)
 			return
-		case strings.Contains(url, "/settleRequest"):
+		case urlParts[2] == "deny" && len(urlParts) == 3:
+			// deny claim route "/claim/1424/deny"
 			doSomething(w, r)
 			return
-		case strings.Contains(url, "/settle"):
+		case urlParts[2] == "settleRequest" && len(urlParts) == 3:
+			// settle claim request route "/claim/1424/settleRequest"
 			doSomething(w, r)
 			return
-		case strings.Contains(url, "/deadline"):
+		case urlParts[2] == "settle" && len(urlParts) == 3:
+			// settle claim route "/claim/1424/settle"
 			doSomething(w, r)
 			return
-		case strings.Contains(url, "/interest"):
+		case urlParts[2] == "deadline" && len(urlParts) == 3:
+			// set deadline for claim route "/claim/1424/deadline"
 			doSomething(w, r)
 			return
-		case !strings.Contains(util.RemoveFirstUrlPathElement(url), "/"):
+		case urlParts[2] == "interest" && len(urlParts) == 3:
+			// set interest for claim route "/claim/1424/interest"
 			doSomething(w, r)
 			return
 		default:
@@ -57,5 +71,5 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func doSomething(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Call to %s", r.URL.String())
+	fmt.Printf("Call to %s\n", r.URL.String())
 }
